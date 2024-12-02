76c18e8c0adbad614f7f2ea17d54dec901acc518
vina786 <vinay76b@gmail.com>
2024-12-02 15:03:13 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class TypeCasting{
+    public static void main(String[] args){
+        int a=50;
+        char b=(char)(a);
+        char c='7';
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
