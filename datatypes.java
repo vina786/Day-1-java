9740fb197676124fc054f516da48f3470c9fe639
vina786 <vinay76b@gmail.com>
2024-12-02 14:42:13 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class DataTypes
+{
+    public static void main(String[] args)
+    {
+        int a=12;
+        char b='A';
+        float c=3.14f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    
+    }
+}
